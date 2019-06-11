75c5a9516c0713ae3c74a0ad5dcbb117e7e11fee
supersmitty2018 <48794241+supersmitty2018@users.noreply.github.com>
2019-06-11 10:13:37 +0200

    Episode 13

---
+++ resources/views/projects/edit.blade.php
@@ -0,0 +1,42 @@
+@extends('layout')
+
+@section('content');
+    <h1>Edit project</h1>
+
+<form action="/projects/ {{ $project->id }}" method="POST">
+
+    {{ method_field("PATCH") }};
+    {{ csrf_field() }};
+
+    <div>
+        <label for="title">Title</label>
+            <div>
+                <input type="text" name='title'  placeholder='title' value=' {{ $project->title }}'>
+            </div>
+    </div>
+
+    <div>
+        <label for="description">Description</label>
+    </div>
+    <div>
+        <textarea name="description" >{{ $project->description }}</textarea>
+    </div>
+
+<div>
+    <div>
+        <button type="submit">Update Project</button>
+    </div>
+</div>
+</form>
+
+<form method='POST' action="/projects/ {{ $project->id }}" >
+    {{ method_field('DELETE') }};
+    {{ csrf_field() }};
+
+<div>
+        <button type="submit">Delete Project</button>
+    </div>
+
+</form>
+
+@endsection
